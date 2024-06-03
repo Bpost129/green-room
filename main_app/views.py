c24423507a329aeab52a69bbe35156191f91af61
Bpost129 <Bpost129@yahoo.com>
2024-06-03 01:11:24 -0400

    render list of plants with temporary styling

--- main_app/views.py
+++ main_app/views.py
@@ -15,3 +15,13 @@ class Plant:
     self.type = type
     self.height = height
     self.age = age
+
+plants = [
+  Plant('Barry', 'Bush', 2, 7),
+  Plant('Terry', 'Tree', 148, 700),
+  Plant('Larry', 'Lilac', 1, 3),
+  Plant('Gary', 'Grass', 0, 15),
+]
+
+def plant_index(request):
+  return render(request, 'plants/index.html', { 'plants': plants })
